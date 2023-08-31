c49ede3f70d4516d62aca78ff3e884fa7aebb729
loganmceldowney <daniel.mceldowney@ucdenver.edu>
2023-08-31 16:18:45 -0600

    removed bug

--- scripts/script.js
+++ scripts/script.js
@@ -44,34 +44,6 @@ btnNavEl.addEventListener("click", function () {
 });
 
 ///////////////////////////////////////////////////////////
-// Smooth Scrolling Animation
-const allLinks = document.querySelectorAll("a:link");
-allLinks.forEach(function (link) {
-  link.addEventListener("click", function (e) {
-    e.preventDefault();
-    const href = link.getAttribute("href");
-
-    // Scroll back to top
-    if (href === "#")
-      window.scrollTo({
-        top: 0,
-        behavior: "smooth",
-      });
-
-    // Scroll to other links
-    if (href !== "#" && href.startsWith("#")) {
-      const sectionEl = document.querySelector(href);
-      sectionEl.scrollIntoView({ behavior: "smooth" });
-    }
-
-    // Close mobile navigation
-    if (link.classList.contains("main-nav-link")) {
-      headerEl.classList.toggle("nav-open");
-    }
-  });
-});
-
-///////////////////////////////////////////////////////////
 // Set Current Year
 const yearEl = document.querySelector(".year");
 const currentYear = new Date().getFullYear();
