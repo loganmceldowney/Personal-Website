1a7dc4973b1f3a580df11ca111eb4257ef091e2c
loganmceldowney <daniel.mceldowney@ucdenver.edu>
2023-08-31 04:25:44 -0600

    Added mobile navigation

---
+++ scripts/script.js
@@ -0,0 +1,44 @@
+///////////////////////////////////////////////////////////
+// Sticky Navigation
+
+const sectionHeroEl = document.querySelector(".section-hero");
+
+const obs = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+    console.log(ent);
+
+    if (ent.isIntersecting === false) {
+      document.body.classList.add("sticky");
+    }
+
+    if (ent.isIntersecting === true) {
+      document.body.classList.remove("sticky");
+    }
+  },
+  {
+    // In the viewport
+    root: null,
+    threshold: 0,
+    rootMargin: "-80px",
+  }
+);
+obs.observe(sectionHeroEl);
+
+///////////////////////////////////////////////////////////
+// Make mobile navigation work
+const btnNavEl = document.querySelector(".btn-mobile-nav");
+const headerEl = document.querySelector(".header");
+const linksEl = document.querySelectorAll(".main-nav-link");
+
+console.log(linksEl);
+btnNavEl.addEventListener("click", function () {
+  headerEl.classList.toggle("nav-open");
+  if (headerEl.classList.contains("nav-open")) {
+    linksEl.forEach((link) => {
+      link.addEventListener("click", function () {
+        headerEl.classList.remove("nav-open");
+      });
+    });
+  }
+});
